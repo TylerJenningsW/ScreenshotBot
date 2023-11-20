392a8c085f193c807569e0a3f553976a17618f21
Tyler Jennings <tylerjennings2@outlook.com>
2023-11-20 00:22:08 -0500

    Added screenshot command based on twitter/x url

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 import { Client } from 'discord.js'
+import puppeteer from 'puppeteer'
+import { generateBrowserSettings } from './puppeteer/browserSettings'
 
 const client = new Client({
   intents: ['Guilds', 'GuildMessages', 'GuildMembers', 'MessageContent'],
@@ -8,5 +10,40 @@ const client = new Client({
 client.on('ready', (c) => {
   console.log(`${c.user.username} is online.`)
 })
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return
 
+  const twitterUrlRegex =
+    /https?:\/\/twitter\.com\/[a-zA-Z0-9_]{1,15}\/status\/\d+/
+  const xUrlRegex = /https?:\/\/x\.com\/[a-zA-Z0-9_]{1,15}\/status\/\d+/
+  
+  const urlMatches = message.content.match(twitterUrlRegex)
+  const urlMatchesX = message.content.match(xUrlRegex)
+  let url = null
+  if (urlMatches) {
+    url = urlMatches[0]
+  } else if (urlMatchesX) {
+    url = urlMatchesX[0]
+  }
+  if (url !== null) {
+    try {
+      const browser = await puppeteer.launch({
+        args: generateBrowserSettings(),
+      })
+
+      const page = await browser.newPage()
+      await page.goto(url as string, { waitUntil: 'networkidle2' })
+
+      const screenshotBuffer = await page.screenshot()
+      await message.reply({
+        files: [{ attachment: screenshotBuffer, name: 'screenshot.png' }],
+      })
+
+      await browser.close()
+      url = null
+    } catch (error) {
+      console.error('Error taking screenshot:', error)
+    }
+  }
+})
 client.login(process.env.TOKEN)
