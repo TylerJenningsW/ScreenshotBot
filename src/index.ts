ccb23b91490fd4627293919494f0875632cb01b0
Tyler Jennings <tylerjennings2@outlook.com>
2023-11-22 10:07:20 -0500

    Replicated original twitter embed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config'
 import { Client } from 'discord.js'
-import puppeteer from 'puppeteer'
+import puppeteer, { ElementHandle } from 'puppeteer'
 import { generateBrowserSettings } from './puppeteer/browserSettings'
+import { EmbedBuilder } from 'discord.js'
 
 const client = new Client({
   intents: ['Guilds', 'GuildMessages', 'GuildMembers', 'MessageContent'],
@@ -14,16 +15,19 @@ client.on('messageCreate', async (message) => {
   if (message.author.bot) return
 
   const twitterUrlRegex =
-    /https?:\/\/twitter\.com\/[a-zA-Z0-9_]{1,15}\/status\/\d+/
-  const xUrlRegex = /https?:\/\/x\.com\/[a-zA-Z0-9_]{1,15}\/status\/\d+/
+    /https?:\/\/twitter\.com\/([a-zA-Z0-9_]{1,15})\/status\/\d+/
+  const xUrlRegex = /https?:\/\/x\.com\/([a-zA-Z0-9_]{1,15})\/status\/\d+/
 
   const urlMatches = message.content.match(twitterUrlRegex)
   const urlMatchesX = message.content.match(xUrlRegex)
   let url = null
+  let user = null
   if (urlMatches) {
     url = urlMatches[0]
+    user = 'https://twitter.com/' + urlMatches[1]
   } else if (urlMatchesX) {
     url = urlMatchesX[0]
+    user = 'https://x.com/' + urlMatchesX[1]
   }
   if (url !== null) {
     try {
@@ -40,6 +44,7 @@ client.on('messageCreate', async (message) => {
         width: 1440,
         height: 900,
       })
+      let avatarUrl = null
       await page.goto(url as string, { waitUntil: 'networkidle2' })
       await page.evaluate(() => {
         let element = document.body
@@ -54,10 +59,90 @@ client.on('messageCreate', async (message) => {
           element.style.display = 'none'
         }
       })
-      const screenshotBuffer = await page.screenshot()
-      await message.reply({
-        files: [{ attachment: screenshotBuffer, name: 'screenshot.png' }],
-      })
+
+      const extractedText = await page.$eval(
+        '*',
+        (el) => (el as HTMLElement).innerText
+      )
+
+      console.log(extractedText)
+      let arrayText = extractedText.split('\n')
+      let name = arrayText[4]
+      let handle = arrayText[5]
+      let tweet = ''
+      let tweetIndex = 0
+      let timeIndex = 0
+      let time = null
+      let likesIndex = 0
+      let likes = null
+      let rtIndex = 0
+      let retweet = null
+      for (let i = 6; i < arrayText.length; i++) {
+        if (arrayText[i] == '·' && arrayText[i + 2] == ' Views') {
+          tweetIndex = i - 2
+          timeIndex = i - 1
+          likesIndex = i + 7
+          rtIndex = i + 3
+          break
+        }
+      }
+      time = arrayText[timeIndex]
+      likes = arrayText[likesIndex]
+      retweet = arrayText[rtIndex]
+      for (let i = 6; i < tweetIndex + 1; i++) {
+        tweet = tweet.concat(arrayText[i])
+        tweet = tweet.concat('\n')
+      }
+
+      let screenshotBuffer = null
+      const articleElement = await page.$('article')
+      if (articleElement) {
+        const avatars = (await page.$x(
+          '(//article//img)[1]'
+        )) as ElementHandle<Element>[]
+
+        const [imgElement] = await page.$x('(//article//img)[2]')
+        if (imgElement) {
+          screenshotBuffer = await imgElement.screenshot()
+        } else {
+          console.log('No img tag found within the article')
+        }
+        if (avatars.length > 0) {
+          const avatar: ElementHandle<Element> = avatars[0]
+          const imgSrc: string | null = await avatar.evaluate((el) =>
+            el.getAttribute('src')
+          )
+          avatarUrl = imgSrc
+          console.log(imgSrc)
+        } else {
+          console.log('No img tag found within the article')
+        }
+      } else {
+        console.log('Article tag not found')
+      }
+      const embed = new EmbedBuilder()
+        .setAuthor({
+          name: name + ' (' + handle + ')',
+          iconURL: avatarUrl!,
+          url: user!,
+        })
+        .setDescription(tweet)
+        .setImage('attachment://screenshot.png')
+        .setFooter({ text: 'Twitter • ' + time })
+        .addFields(
+          { name: 'Likes', value: likes, inline: true },
+          { name: 'Retweets', value: retweet, inline: true }
+        )
+      if (screenshotBuffer !== null) {
+        await message.reply({
+          embeds: [embed],
+          files: [{ attachment: screenshotBuffer, name: 'screenshot.png' }],
+        })
+      } else {
+        await message.reply({
+          embeds: [embed],
+        })
+      }
 
       await browser.close()
       url = null
